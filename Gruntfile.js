c9544a4a67364b75d74181ae1bb72ff03e3401c0
Alex Tran <vietnogi@gmail.com>
2015-05-15 09:43:58 -0700

    coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,12 +137,11 @@ module.exports = function (grunt) {
 
     coveralls: {
       options: {
-        // LCOV coverage file relevant to every target
-        src: 'coverage/**/lcov.info',
-        // When true, grunt-coveralls will only print a warning rather than
-        // an error, to prevent CI builds from failing unnecessarily (e.g. if
-        // coveralls.io is down). Optional, defaults to false.
-        force: false
+        debug: true,
+        coverageDir: 'coverage',
+        dryRun: false,
+        force: true,
+        recursive: true
       }
     },
 
